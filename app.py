5a09f1db516751816a047c23559bf085d0602583
Kjell Erik Reed Anda <keaaa@equinor.com>
2024-01-18 15:02:45 +0100

    update requirements

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ import asyncio
 import json
 import math
 import os
+import time
 
 import avro.schema
 
 from avroserializer import write_avro_file_with_schema
 from eventhub import send_to_event_hub
 
-json_path = "feature_short.json"
+json_path = "feature.json"
 schema_path = "feature.avsc"
 conn_str = os.getenv("EVENT_HUB_CONN_STR")
 
@@ -25,6 +26,20 @@ def create_nan_issue():
     return write_avro_file_with_schema(content, schema)
 
 
+async def simulate_load(sample_time_interval_sec, run_for_sec):
+    schema = avro.schema.parse(open(schema_path).read())
+    start_time = int(time.time() * 1000000000)
+    run_to_time = start_time + run_for_sec * 1000000000
+    content = get_json(json_path)
+    while start_time < run_to_time:
+        content["measurementStartTime"] = start_time
+        avro_binary = write_avro_file_with_schema(content, schema)
+        await send_to_event_hub(conn_str, avro_binary)
+        time.sleep(sample_time_interval_sec)
+        start_time = int(time.time() * 1000000000)
+
+
 avro_as_bytes = create_nan_issue()
 loop = asyncio.get_event_loop()
-loop.run_until_complete(send_to_event_hub(conn_str, avro_as_bytes))
+# loop.run_until_complete(send_to_event_hub(conn_str, avro_as_bytes))
+loop.run_until_complete(simulate_load(1, 120))
